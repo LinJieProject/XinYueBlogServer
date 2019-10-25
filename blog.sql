8f9b5f06268213340b15018975b926db5114cb18
lddz <260379295@qq.com>
2019-10-25 20:26:27 +0800

    修改了文章表及sql文件

--- blog.sql
+++ blog.sql
@@ -3,15 +3,15 @@
 
  Source Server         : MySQL
  Source Server Type    : MySQL
- Source Server Version : 80018
+ Source Server Version : 50727
  Source Host           : localhost:3306
  Source Schema         : blog
 
  Target Server Type    : MySQL
- Target Server Version : 80018
+ Target Server Version : 50727
  File Encoding         : 65001
 
- Date: 24/10/2019 11:46:37
+ Date: 25/10/2019 20:25:14
 */
 
 SET NAMES utf8mb4;
@@ -21,44 +21,43 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for article
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
-CREATE TABLE `article` (
+CREATE TABLE `article`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '文章ID',
-  `tag_id` bigint(20) unsigned NOT NULL COMMENT '标签ID',
-  `content` longtext COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章内容',
-  `title` varchar(1024) COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章标题',
-  `view_count` int(255) unsigned NOT NULL COMMENT '阅读次数',
-  `comment_count` int(255) unsigned NOT NULL COMMENT '评论次数',
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章内容',
+  `title` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章标题',
+  `view_count` int(255) UNSIGNED NOT NULL COMMENT '阅读次数',
+  `comment_count` int(255) UNSIGNED NOT NULL COMMENT '评论次数',
   `username` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章作者',
-  `summary` varchar(256) COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章摘要',
-  `create_time` timestamp NOT NULL COMMENT '发布时间',
-  `update_time` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `summary` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章摘要',
+  `create_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '发布时间',
+  `update_time` timestamp(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for comment
 -- ----------------------------
 DROP TABLE IF EXISTS `comment`;
-CREATE TABLE `comment` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '评论ID',
-  `content` text COLLATE utf8mb4_general_ci NOT NULL COMMENT '评论内容',
-  `username` varchar(64) COLLATE utf8mb4_general_ci NOT NULL COMMENT '评论作者',
-  `create_time` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '发布时间',
-  `article_id` bigint(20) unsigned NOT NULL COMMENT '对应文章ID',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `comment`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '评论ID',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '评论内容',
+  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '评论作者',
+  `create_time` timestamp(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '发布时间',
+  `article_id` bigint(20) UNSIGNED NOT NULL COMMENT '对应文章ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TABLE `user`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '表ID',
   `user_id` bigint(20) NOT NULL COMMENT '用户ID',
-  `username` varchar(64) COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
-  `password` varchar(64) COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
-  `gender` tinyint(4) NOT NULL DEFAULT '0' COMMENT '性别',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
+  `gender` tinyint(4) NOT NULL DEFAULT 0 COMMENT '性别',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
