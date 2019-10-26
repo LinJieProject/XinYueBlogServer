95a38a7ab2883f827d5d1019d2f7c0a4aa0d39ef
lddz <2603792595@qq.com>
2019-10-26 12:04:41 +0800

    添加几条用户数据记录，修改了sql文件

--- blog.sql
+++ blog.sql
@@ -3,15 +3,15 @@
 
  Source Server         : MySQL
  Source Server Type    : MySQL
- Source Server Version : 50727
+ Source Server Version : 80018
  Source Host           : localhost:3306
  Source Schema         : blog
 
  Target Server Type    : MySQL
- Target Server Version : 50727
+ Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 25/10/2019 21:34:35
+ Date: 26/10/2019 12:03:50
 */
 
 SET NAMES utf8mb4;
@@ -21,49 +21,59 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for article
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
-CREATE TABLE `article`  (
+CREATE TABLE `article` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '文章ID',
   `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章内容',
   `title` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章标题',
-  `view_count` int(255) UNSIGNED NOT NULL COMMENT '阅读次数',
-  `comment_count` int(255) UNSIGNED NOT NULL COMMENT '评论次数',
+  `view_count` int(255) unsigned NOT NULL COMMENT '阅读次数',
+  `comment_count` int(255) unsigned NOT NULL COMMENT '评论次数',
   `username` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章作者',
   `summary` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章摘要',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of article
 -- ----------------------------
+BEGIN;
 INSERT INTO `article` VALUES (1, '内容', 'Go语言基础之结构体', 0, 0, 'q1mi', 'Go语言中没有“类”的概念，也不支持“类”的继承等面向对象的概念。Go语言中通过结构体的内嵌再配合接口比面向对象具有更高的扩展性和灵活性。');
 INSERT INTO `article` VALUES (2, '内容', 'Go语言基础之包', 0, 0, 'q1mi', '在工程化的Go语言开发项目中，Go语言的源码复用是建立在包（package）基础之上的。本文介绍了Go语言中如何定义包、如何导出包的内容及如何导入其他包。');
 INSERT INTO `article` VALUES (3, '内容', 'Go语言基础之反射', 0, 0, 'q1mi', '本文介绍了Go语言反射的意义和基本使用。');
 INSERT INTO `article` VALUES (4, '内容', 'Gin框架介绍及使用', 0, 0, 'q1mi', 'Gin是一个用Go语言编写的web框架。它是一个类似于martini但拥有更好性能的API框架, 由于使用了httprouter，速度提高了近40倍。 如果你是性能和高效的追求者, 你会爱上Gin。');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for comment
 -- ----------------------------
 DROP TABLE IF EXISTS `comment`;
-CREATE TABLE `comment`  (
-  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '评论ID',
+CREATE TABLE `comment` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '评论ID',
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '评论内容',
   `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '评论作者',
-  `create_time` timestamp(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '发布时间',
-  `article_id` bigint(20) UNSIGNED NOT NULL COMMENT '对应文章ID',
+  `create_time` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '发布时间',
+  `article_id` bigint(20) unsigned NOT NULL COMMENT '对应文章ID',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
+CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '表ID',
   `user_id` bigint(20) NOT NULL COMMENT '用户ID',
   `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
   `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
-  `gender` tinyint(4) NOT NULL DEFAULT 0 COMMENT '性别',
+  `gender` tinyint(4) NOT NULL DEFAULT '0' COMMENT '性别',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` VALUES (1, 123, 'tom', '123', 1);
+INSERT INTO `user` VALUES (2, 124, 'sam', '111', 0);
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
